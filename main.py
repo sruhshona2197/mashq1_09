9ed3bc2b55faae6a5bfacdbe29c29329cc641022
sruhshona2197 <sruhshona2197@gmail.com>
2025-12-09 16:53:00 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# 1-mashq
+kvadratlar = [i**2 for i in range(1, 11)]
+print(kvadratlar)
+
+
+# 2-mashq
+juft_sonlar = [i for i in range(1, 21) if i % 2 == 0]
+print(juft_sonlar)
+
+
+# 3-mashq
+toq_sonlar = [i for i in range(10, 31) if i % 2 == 1]
+print(toq_sonlar)
+
+
+# 4-mashq
+uzunliklar = [len(i) for i in ["apple", "banana", "pear"]]
+print(uzunliklar)
+
+
+# 5-mashq
+harflar = [harf for harf in "So'z"]
+print(harflar)
+
+
+# 6-mashq
+kublar = [i**3 for i in range(1, 6)]
+print(kublar)
